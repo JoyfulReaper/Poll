63cf79536ae505a5bf5fd33b30722376033a36de
Kyle Givler <xvegan88x@gmail.com>
2021-01-18 16:11:54 -0500

    Comment this out for now
    Will delete it later anyway

--- QuickTest/Program.cs
+++ QuickTest/Program.cs
@@ -12,101 +12,101 @@ namespace QuickTest
     {
         static void Main(string[] args)
         {
-            MainAsync().GetAwaiter().GetResult();
+            //MainAsync().GetAwaiter().GetResult();
         }
 
         private static async Task MainAsync()
         {
-            DbContextOptionsBuilder optionsBuilder = new DbContextOptionsBuilder();
-            optionsBuilder.UseSqlServer("Server=RyzenShine;Database=Poll;Trusted_Connection=True;");
+        //    DbContextOptionsBuilder optionsBuilder = new DbContextOptionsBuilder();
+        //    optionsBuilder.UseSqlServer("Server=RyzenShine;Database=Poll;Trusted_Connection=True;");
 
-            using (var context = new PollContext(optionsBuilder.Options))
-            {
-                PollData data = new PollData(context);
+        //    using (var context = new PollContext(optionsBuilder.Options))
+        //    {
+        //        PollData data = new PollData(context);
 
-                await LibAdd(data);
-                //await LibRemove(data);
+        //        await LibAdd(data);
+        //        //await LibRemove(data);
 
-                //var test = await data.GetAllPolls();
-                //var test2 = await data.GetPollById(1);
+        //        //var test = await data.GetAllPolls();
+        //        //var test2 = await data.GetPollById(1);
 
-                Console.WriteLine("Hello World!");
-            }
-        }
+        //        Console.WriteLine("Hello World!");
+        //    }
+        //}
 
-        private static async Task LibAdd(PollData data)
-        {
-            Poll newPoll = new Poll()
-            {
-                Name = "Another Test Poll2",
-                Options = GetDemoOptions(),
-                Votes = GetDemoVotes(),
-                Context = new Context { Name = "Testing" }
-            };
+        //private static async Task LibAdd(PollData data)
+        //{
+        //    Poll newPoll = new Poll()
+        //    {
+        //        Name = "Another Test Poll2",
+        //        Options = GetDemoOptions(),
+        //        Votes = GetDemoVotes(),
+        //        Context = new Context { Name = "Testing" }
+        //    };
 
-            await data.AddPoll(newPoll);
-        }
+        //    await data.AddPoll(newPoll);
+        //}
 
-        private static async Task LibRemove(PollData data)
-        {
-            Poll poll = new Poll()
-            {
-                Id = 1,
-                Name = "Test Poll",
-                Options = GetDemoOptions(),
-                Votes = GetDemoVotes(),
-                Context = new Context { Name = "Testing" }
-            };
+        //private static async Task LibRemove(PollData data)
+        //{
+        //    Poll poll = new Poll()
+        //    {
+        //        Id = 1,
+        //        Name = "Test Poll",
+        //        Options = GetDemoOptions(),
+        //        Votes = GetDemoVotes(),
+        //        Context = new Context { Name = "Testing" }
+        //    };
 
-            await data.RemovePoll(poll);
-        }
+        //    await data.RemovePoll(poll);
+        //}
 
-        private static List<Vote> GetDemoVotes()
-        {
-            List<Vote> votes = new List<Vote>();
-            votes.Add(new Vote
-            {
-                User = new User { UserName = "TestUser"},
-                Option = new Option { Name = "Option 1" }
-            });
+        //private static List<Vote> GetDemoVotes()
+        //{
+        //    List<Vote> votes = new List<Vote>();
+        //    votes.Add(new Vote
+        //    {
+        //        User = new User { UserName = "TestUser"},
+        //        Option = new Option { Name = "Option 1" }
+        //    });
 
-            return votes;
-        }
+        //    return votes;
+        //}
 
-        private static List<Option> GetDemoOptions()
-        {
-            List<Option> options = new List<Option>();
-            options.Add(new Option
-            {
-                Name = "Option 1"
-            });
-            options.Add(new Option
-            {
-                Name = "Option 2"
-            });
+        //private static List<Option> GetDemoOptions()
+        //{
+        //    List<Option> options = new List<Option>();
+        //    options.Add(new Option
+        //    {
+        //        Name = "Option 1"
+        //    });
+        //    options.Add(new Option
+        //    {
+        //        Name = "Option 2"
+        //    });
 
-            return options;
-        }
+        //    return options;
+        //}
 
-        private static void AddToDbTest(DbContextOptionsBuilder optionsBuilder)
-        {
-            using (var context = new PollContext(optionsBuilder.Options))
-            {
-                Poll poll = new Poll
-                {
-                    Name = "test",
-                    Options = GetDemoOptions(),
-                    Votes = GetDemoVotes(),
-                    Context = new Context { Name = "TESTING" }
-                };
+        //private static void AddToDbTest(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    using (var context = new PollContext(optionsBuilder.Options))
+        //    {
+        //        Poll poll = new Poll
+        //        {
+        //            Name = "test",
+        //            Options = GetDemoOptions(),
+        //            Votes = GetDemoVotes(),
+        //            Context = new Context { Name = "TESTING" }
+        //        };
 
-                context.Add(poll);
-                context.SaveChanges();
+        //        context.Add(poll);
+        //        context.SaveChanges();
 
-                Poll pollfromDb = context.Polls.Include(a => a.Options).Include(a => a.Votes).Where(x => x.Id == 1).FirstOrDefault();
+        //        Poll pollfromDb = context.Polls.Include(a => a.Options).Include(a => a.Votes).Where(x => x.Id == 1).FirstOrDefault();
 
-                Console.WriteLine(pollfromDb.Name);
-            }
+        //        Console.WriteLine(pollfromDb.Name);
+        //    }
         }
     }
 }
